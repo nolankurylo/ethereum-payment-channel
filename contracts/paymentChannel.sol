46f1d79d4c1b024531c8848a04203263da7260a6
"Nolan Kurylo" <"kurylon@telus.net">
2022-07-28 11:54:46 -0700

    cleaned up code and scripts

--- contracts/paymentChannel.sol
+++ contracts/paymentChannel.sol
@@ -12,8 +12,8 @@ contract PaymentChannel {
   address payable public messageReceiver;
   uint public contractEndTime;
   uint256 public messageValue;
+  uint256 public contractEscrowedValue;
   bytes32 public merkleTreeRoot;
-  uint256 public maxSendAmount;
 
   //State Machine
   enum States {HandShake,Accepting,InTransfer}
@@ -21,6 +21,8 @@ contract PaymentChannel {
 
   event TransferComplete(uint amountTransferred);
   event TransferFailed(uint amountTransferred);
+  event ValidMicroPayment();
+  event InvalidMicroPayment();
 
   modifier checkMessageSender(){
     require(msg.sender == messageSender, "You are not the correct sender.");
@@ -42,30 +44,32 @@ contract PaymentChannel {
     _;
   }
 
-  //Constructor
-  constructor() payable
-  {
+  /** @dev Constructor
+  *   @param _messageValue how much each micropayment is worth
+  */
+  constructor(uint256 _messageValue) payable
+  { 
+    require(msg.value > 0, "Need an amount to hold in escrow.");
+    contractEscrowedValue = msg.value;
     messageSender = payable(msg.sender);
+    messageValue = _messageValue;
     state = States.HandShake;
-    maxSendAmount = msg.value * 1000000000000000000; // in ether
   }
 
   /**
   * @dev Function for MessageSender to initiate payment channel between MessageSender and MessageReceiver
   * @param _messageReceiver address of receipient
   * @param _validityTime number of minutes before the contract expires 
-  * @param _messageValue how much each micropayment is worth
   * @param _merkleTreeRoot hash of merkle tree root from sender
   */
-  function handShake(address payable _messageReceiver, uint _validityTime, uint256 _messageValue, bytes32 _merkleTreeRoot) public
+  function handShake(address payable _messageReceiver, uint _validityTime, bytes32 _merkleTreeRoot) public
     payable
     checkMessageSender
     checkState(States.HandShake)
   {
     messageReceiver = _messageReceiver;
-    require(block.timestamp < block.timestamp + _validityTime * 1 minutes, "Handsake duration invalid");
-    contractEndTime = block.timestamp + _validityTime * 1 minutes;
-    messageValue = _messageValue;
+    require(block.timestamp < block.timestamp + _validityTime * 1 seconds, "Handsake duration invalid");
+    contractEndTime = block.timestamp + _validityTime * 1 seconds;
     merkleTreeRoot = _merkleTreeRoot;
     state = States.Accepting;
   }
@@ -76,7 +80,7 @@ contract PaymentChannel {
   * @param _microPaymentHash hash of micropayment from sender
   * @param _microPaymentNumber which micropayment
   */
-  function claim(bytes32 _microPaymentHash, uint256 _microPaymentNumber) public payable
+  function claimPayment(bytes32 _microPaymentHash, uint256 _microPaymentNumber) public payable
     checkMessageReceiver
     checkState(States.Accepting)
   {
@@ -89,12 +93,12 @@ contract PaymentChannel {
 
     if(hash != merkleTreeRoot) {
       state = States.Accepting;
-      emit TransferFailed(maxSendAmount);
+      emit TransferFailed(contractEscrowedValue);
       revert();
     }
 
     uint sendAmount = _microPaymentNumber * messageValue * 1000000000000000000;
-    require(sendAmount <= maxSendAmount, "Trying to send more than what was defined at contract deployment");
+    require(sendAmount <= contractEscrowedValue, "Trying to send more than what was defined at contract deployment");
 
     
     if (messageReceiver.send(sendAmount)) {
@@ -108,17 +112,6 @@ contract PaymentChannel {
     
   }
 
-  /**
-  * @dev Function for MessageSender to extend the expiration time of the contract
-  * @param _validityTime number of minutes to add to contract's current exipiration time
-  */
-  function updateExpiration(uint _validityTime) public
-    checkMessageSender
-    checkState(States.Accepting)
-    {
-      require(contractEndTime < contractEndTime + _validityTime * 1 minutes, "Can't update to new expiration, invalid new expiration");
-      contractEndTime += _validityTime * 1 minutes;
-    }
 
   /**
   * @dev Function for MessageSender to obtain a refund if the contract has expired and the MessageReceiver has not collected payment
---
+++ scripts/offlineMicroPayments.js
@@ -0,0 +1,65 @@
+(async () => {
+  try {
+
+    // USER DEFINED INPUT ----------------------------------------------------------
+    let MESSAGE = "sdfgsdfsd"
+    let NUMBER_MICRO_PAYMENTS = 10 // unknown to reciever
+    let MICRO_PAYMENT_VALUE = 1 // unknown to reciever
+    let TOTAL_PAYMENT_VALUE = NUMBER_MICRO_PAYMENTS * MICRO_PAYMENT_VALUE
+    // USER DEFINED INPUT ----------------------------------------------------------
+
+    // Note that the script needs the ABI which is generated from the compilation artifact.
+    const metadata = JSON.parse(await remix.call('fileManager', 'getFile', 'contracts/artifacts/PaymentChannel.json'))
+    const accounts = await web3.eth.getAccounts()
+
+    let senderAccount = accounts[1]
+    let receiverAccount = accounts[2]
+
+    let senderBalance = await web3.eth.getBalance(senderAccount);
+    let senderWallet = web3.utils.fromWei(senderBalance, "ether");
+
+    var receiverBalance = await web3.eth.getBalance(receiverAccount);
+    var receiverWallet = web3.utils.fromWei(receiverBalance, "ether");
+
+    console.log(`Sender: ${senderAccount}, Receiver: ${receiverAccount}`)
+    console.log(`BEFORE CONTRACT DEPLOYMENT: SenderBalance: ${senderWallet}, ReceiverBalance: ${receiverWallet}`)
+    console.log(`Message to encrypt: ${MESSAGE}`)
+
+    // Create the Merkle Tree
+    merkleTree = {}
+
+    var messageHash = web3.utils.sha3(MESSAGE, {encoding: 'ascii'});
+
+    merkleTree[NUMBER_MICRO_PAYMENTS] = messageHash
+    console.log(`MerkleTree of ${NUMBER_MICRO_PAYMENTS} MicroPayments:`);
+    console.log(`${NUMBER_MICRO_PAYMENTS}: ${messageHash}`);
+    for(var i = 1; i < NUMBER_MICRO_PAYMENTS; i++) {
+      messageHash = web3.utils.sha3(messageHash, {encoding: 'hex'});
+      console.log(`${NUMBER_MICRO_PAYMENTS - i}: ${messageHash}`);
+      
+      merkleTree[NUMBER_MICRO_PAYMENTS-i] = messageHash
+    }
+
+    merkleTree['root'] = web3.utils.sha3(messageHash, {encoding: 'hex'});
+
+    console.log(`The MerkleRoot is: ${merkleTree['root']}`);
+    console.log(`The total value to be held by the contract is: ${TOTAL_PAYMENT_VALUE} ether`);
+    
+    
+    let contract = new web3.eth.Contract(metadata.abi)
+    contract = contract.deploy({
+      data: metadata.data.bytecode.object,
+      arguments: [MICRO_PAYMENT_VALUE]
+    })
+
+    newContractInstance = await contract.send({
+      from: senderAccount,
+      value: TOTAL_PAYMENT_VALUE * 1000000000000000000
+
+    })
+
+  } catch (e) {
+    console.log(e.message)
+  }
+})()
+
